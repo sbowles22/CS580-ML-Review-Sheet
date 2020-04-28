faf2ed3304f30db10dc9ff9206643bf2acbb13bd
JansherA <43053017+JansherA@users.noreply.github.com>
2020-04-28 09:53:32 -0400

    Create website.js

---
+++ website.js
@@ -0,0 +1,17 @@
+//NOT MY CODE NOR IS IT NECESSARY, JUST MAKES SCROLLING NICE
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
